e9e6ae2993d912f2f522b87cdcee5d2b5f21ed69
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-07-05 09:12:12 -0700

    Refocus this driver on the i2c configuration interface

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 
-# mt9v034
+# mt9v034-i2c
 
 Rust no_std driver for the 
 ON Semiconductor MT9V034 image sensor
 
-This is a low-cost global shutter CMOS image sensor useful for robotics.
+This is a low-cost global shutter CMOS image sensor useful for robotics
+and machine vision applications.
+
+This driver specifically interfaces with the sensor's
+two-wire (i2c) programming interface, which allows you to set
+various camera configuration parameters.  This driver does not obtain
+pixel data from the camera.  For that you will need a driver that 
+pulls data from the camera's parallel data lines.  (Example forthcoming in another crate.)
+
+## License
+
+BSD-3-Clause: see LICENSE file
+
+## Status
+
+- [ ] Access to basic configuration registers
+- [ ] Support for register locking (to prevent accidental changes due to i2c noise)
+- [ ] Example for cortex-m
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,12 @@ LICENSE: BSD3 (see LICENSE file)
 */
 #![no_std]
 
-//! Driver for MT9V034 image sensor
+//! Configuration driver for the ON Semiconductor MT9V034 image sensor
+//! This imaging sensor has multiple interfaces:
+//! - Two-wire i2c for configuration registers (i2c)
+//! - parallel pixel data out (dout)
+//! - pixel out sync (vsync, hsync, pix clock)
+//! This driver is concerned only with the i2c interface
 
 #[cfg(feature = "rttdebug")]
 use panic_rtt_core::rprintln;
@@ -21,46 +26,33 @@ pub enum Error<CommE, PinE> {
     Timeout,
 }
 
-/// The number of parallel pixel data lines
-const NUM_DIN_PINS: usize = 10;
+pub const DEFAULT_I2C_ADDRESS: u8 = I2C_WRITE_ADDRESS;
 
-/// Contains the main parallel data interface configuration
-pub struct ParallelPixelPins<DIN, COUT> {
-    /// The pixel data pins
-    din: [DIN; NUM_DIN_PINS],
-    /// pixel data clock
-    pix_clk: COUT,
-    /// horizontal sync / LINE_VALID
-    hsync: COUT,
-    /// vertical sync / FRAME_VALID
-    vsync: COUT,
-}
-
-/// This imaging sensor has multiple interfaces:
-/// - i2c for configuration registers (i2c)
-/// - parallel pixel data out (dout)
-/// - pixel out sync (vsync, hsync, pix clock)
-pub struct Mt9v034<DIN, COUT, I2C> {
-    ppix: ParallelPixelPins<DIN, COUT>,
+/// Main driver struct
+pub struct Mt9v034<I2C> {
+    address: u8,
     i2c: I2C,
 }
 
-impl<DIN, COUT, I2C, CommE> Mt9v034<DIN, COUT, I2C>
+impl<I2C, CommE> Mt9v034<I2C>
 where
     I2C: embedded_hal::blocking::i2c::Write<Error = CommE>
         + embedded_hal::blocking::i2c::Read<Error = CommE>
         + embedded_hal::blocking::i2c::WriteRead<Error = CommE>,
-    COUT: embedded_hal::digital::OutputPin,
 {
-    fn new(ppix: ParallelPixelPins<DIN, COUT>, i2c: I2C) -> Self {
-        Self { ppix, i2c }
+    /// Create a new instance with an i2c address:
+    /// May use DEFAULT_I2C_ADDRESS if in doubt.
+    pub fn new(i2c: I2C, address: u8) -> Self {
+        Self { address, i2c }
     }
 
+    /// Second-stage configuration
     pub fn init(&mut self) -> &mut Self {
         //TODO configure reserved registers per Rev G data sheet table 8
         self
     }
 
+    /// Read a u8 from an 8-bit address
     pub fn read_reg_u8(&mut self, reg: u8) -> Result<u8, crate::Error<(), ()>> {
         let cmd_buf = [reg];
         let mut recv_buf = [0u8];
@@ -71,6 +63,7 @@ where
         Ok(recv_buf[0])
     }
 
+    /// Read a u16 from an 8-bit address
     pub fn read_reg_u16(
         &mut self,
         reg: u8,
@@ -82,6 +75,7 @@ where
         Ok(val)
     }
 
+    /// Write a u8 to an 8-bit address
     pub fn write_reg_u8(
         &mut self,
         reg: u8,
@@ -94,6 +88,7 @@ where
         Ok(())
     }
 
+    /// Write a u16 to an 8-bit address
     pub fn write_reg_u16(
         &mut self,
         reg: u8,
@@ -107,15 +102,49 @@ where
     }
 }
 
+// TODO addd support for alternate addresses
 const I2C_WRITE_ADDRESS: u8 = 0xB8;
 const I2C_READ_ADDRESS: u8 = 0xB9;
 /// Used for reading and writing a second byte on registers
 const FOLLOW_UP_ADDRESS: u8 = 0xF0;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Array format: Wide-VGA, Active 752 H x 480 V
+pub const MAX_FRAME_HEIGHT: u16 = 480;
+pub const MAX_FRAME_WIDTH: u16 = 752;
+
+#[repr(u8)]
+pub enum GeneralRegisters {
+    Version = 0x00,
+    Control = 0x07,
+    SoftReset = 0x0c,
+    HdrEnable = 0x0f,
+    AdcResCtrl = 0x1c,
+    RowNoiseCorrCtrl = 0x70,
+    DigitalTest = 0x7f,
+    TiledDigitalGain = 0x80,
+
+    AgcAecDesiredBin = 0xa5,
+    AecUpdate = 0xa6,
+    AecLowpass = 0xa8,
+    AgcUpdate = 0xa9,
+    AgcLowpass = 0xaa,
+    MaxGain = 0xab,
+    /// Minimum coarse shutter width
+    MinExposure = 0xac,
+    /// Maximum coarse shutter width
+    MaxExposure = 0xad,
+    AecAgcEnable = 0xaf,
+    AgcAecPixelCount = 0xb0,
+}
+
+#[repr(u8)]
+pub enum ContextARegisters {
+    ColumnStart = 0x01,
+    //TODO fill in the rest
+}
+
+#[repr(u8)]
+pub enum ContextBRegisters {
+    ColumnStart = 0xc9,
+    //TODO fill in the rest
 }
