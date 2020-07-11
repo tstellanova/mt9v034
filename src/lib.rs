6f5cba5e7d28412e9de0cc718051f1542f867720
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-07-11 16:36:07 -0700

    set some initial configuration defaults

--- src/lib.rs
+++ src/lib.rs
@@ -36,8 +36,7 @@ pub const CAM_PERIPH_ADDRESS_11: u8 = 0xB8 >> 1;
 pub const PX4FLOW_CAM_ADDRESS: u8 = CAM_PERIPH_ADDRESS_11;
 
 /// The camera i2c address for the Arducam breakout board ("UC-396 Rev. A")
-pub const ARDUCAM_BREAKOUT_ADDRESS:u8 = CAM_PERIPH_ADDRESS_00;
-
+pub const ARDUCAM_BREAKOUT_ADDRESS: u8 = CAM_PERIPH_ADDRESS_00;
 
 /// Main driver struct
 pub struct Mt9v034<I2C> {
@@ -70,22 +69,135 @@ where
         &mut self,
         context: ParamContext,
     ) -> Result<(), crate::Error<CommE>> {
-        self.write_reg_u16(GeneralRegisters::Control as u8, context as u16)
+        self.write_general_reg(GeneralRegister::Control, context as u16)
     }
 
     /// Second-stage configuration
     pub fn setup(&mut self) -> Result<(), crate::Error<CommE>> {
         #[cfg(feature = "rttdebug")]
-        rprintln!("mt9v034-i2c setup start 0x{:x}",self.base_address);
+        rprintln!("mt9v034-i2c setup start 0x{:x}", self.base_address);
 
-        let _version = self.read_reg_u8(GeneralRegisters::ChipVersion as u8)?;
-        self.write_reg_u8(GeneralRegisters::SoftReset as u8, 0b11)?;
+        // probe the device: version should match 0x1324 ?
+        let _version = self.read_reg_u8(GeneralRegister::ChipVersion as u8)?;
 
-        //TODO configure reserved registers per Rev G data sheet table 8
+        // configure settings that apply to all contexts
+        self.set_general_defaults()?;
+        self.set_context_b_defaults()?;
+        self.set_context_a_defaults()?;
+
+        //default to Context A
+        self.set_context(ParamContext::ContextA)?;
+
+        // restart image collection
+        self.write_reg_u8(GeneralRegister::SoftReset as u8, 0b11)?;
 
         #[cfg(feature = "rttdebug")]
         rprintln!("mt9v034-i2c setup done");
         Ok(())
+    }
+
+    //TODO add config methods for setting frame dimensions for context A and context B
+
+    fn write_general_reg(
+        &mut self,
+        reg: GeneralRegister,
+        data: u16,
+    ) -> Result<(), crate::Error<CommE>> {
+        self.write_reg_u16(reg as u8, data)?;
+        Ok(())
+    }
+
+    fn write_context_a_reg(
+        &mut self,
+        reg: ContextARegister,
+        data: u16,
+    ) -> Result<(), crate::Error<CommE>> {
+        self.write_reg_u16(reg as u8, data)?;
+        Ok(())
+    }
+
+    fn write_context_b_reg(
+        &mut self,
+        reg: ContextBRegister,
+        data: u16,
+    ) -> Result<(), crate::Error<CommE>> {
+        self.write_reg_u16(reg as u8, data)?;
+        Ok(())
+    }
+
+    /// Set some reasonable configuration defaults
+    pub fn set_general_defaults(&mut self) -> Result<(), crate::Error<CommE>> {
+        self.write_general_reg(GeneralRegister::RowNoiseCorrCtrl, 0x0101)?; //default noise correction
+        self.write_general_reg(GeneralRegister::AecAgcEnable, 0x0011)?; //enable both AEC and AGC
+        self.write_general_reg(GeneralRegister::HdrEnable, 0x0001)?; // enable HDR
+        self.write_general_reg(GeneralRegister::MinExposure, 0x0001)?;
+        self.write_general_reg(GeneralRegister::MaxExposure, 0x1F4)?;
+
+        self.write_general_reg(GeneralRegister::AgcMaxGain, 0x0010)?;
+        self.write_general_reg(GeneralRegister::AgcAecPixelCount, 64 * 64)?; // use all pixels
+        self.write_general_reg(GeneralRegister::AgcAecDesiredBin, 20)?; //desired luminance
+        self.write_general_reg(GeneralRegister::AdcResCtrl, 0x0303)?; // 12 bit ADC
+
+        self.write_general_reg(GeneralRegister::AecUpdate, 0x02)?;
+        self.write_general_reg(GeneralRegister::AecLowpass, 0x01)?;
+        self.write_general_reg(GeneralRegister::AgcUpdate, 0x02)?;
+        self.write_general_reg(GeneralRegister::AgcLowpass, 0x02)?;
+
+        Ok(())
+    }
+
+    pub fn set_context_b_defaults(
+        &mut self,
+    ) -> Result<(), crate::Error<CommE>> {
+        //TODO calculate frame/line sizes
+        self.write_context_b_reg(ContextBRegister::WindowWidth, 0)?;
+        self.write_context_b_reg(ContextBRegister::WindowHeight, 0)?;
+        self.write_context_b_reg(ContextBRegister::HBlanking, 0)?;
+        self.write_context_b_reg(ContextBRegister::VBlanking, 0)?;
+        self.write_context_b_reg(ContextBRegister::ReadMode, 0x305)?; // row bin 2 col bin 4 enable, (9:8)
+        self.write_context_b_reg(ContextBRegister::ColumnStart, 0)?;
+        self.write_context_b_reg(ContextBRegister::RowStart, 0)?;
+        self.write_context_b_reg(ContextBRegister::CoarseShutter1, 443)?;
+        self.write_context_b_reg(ContextBRegister::CoarseShutter2, 473)?;
+        self.write_context_b_reg(ContextBRegister::CoarseShutterCtrl, 0x0164)?;
+        self.write_context_b_reg(ContextBRegister::CoarseShutterTotal, 480)?;
+
+        Ok(())
+    }
+
+    pub fn set_context_a_defaults(
+        &mut self,
+    ) -> Result<(), crate::Error<CommE>> {
+        //TODO calculate frame/line sizes
+        self.write_context_a_reg(ContextARegister::WindowWidth, 0)?;
+        self.write_context_a_reg(ContextARegister::WindowHeight, 0)?;
+        self.write_context_a_reg(ContextARegister::HBlanking, 0)?;
+        self.write_context_a_reg(ContextARegister::VBlanking, 0)?;
+        self.write_context_a_reg(ContextARegister::ReadMode, 0x30A)?; // row + col bin 4 enable, (9:8) default
+        self.write_context_a_reg(ContextARegister::ColumnStart, 0)?;
+        self.write_context_a_reg(ContextARegister::RowStart, 0)?;
+        self.write_context_a_reg(ContextARegister::CoarseShutter1, 443)?;
+        self.write_context_a_reg(ContextARegister::CoarseShutter2, 473)?;
+        self.write_context_a_reg(ContextARegister::CoarseShutterCtrl, 0x0164)?;
+        self.write_context_a_reg(ContextARegister::CoarseShutterTotal, 480)?;
+
+        Ok(())
+    }
+
+    //TODO enable/disable test pattern with write to reg 0x7f
+
+    /// Write-protect (or unprotect) all writable registers.
+    /// If you enable register protection, then any subsequent writes to registers
+    /// (except the write-protection register itself)
+    /// will not be committed.
+    pub fn protect_all_registers(
+        &mut self,
+        protect: bool,
+    ) -> Result<(), crate::Error<CommE>> {
+        self.write_general_reg(
+            GeneralRegister::RegisterLock,
+            if protect { 0xDEAD } else { 0xBEEF },
+        )
     }
 
     /// Read a u8 from an 8-bit address
@@ -94,7 +206,7 @@ where
         let cmd_buf = [reg];
         let mut recv_buf = [0u8];
         self.i2c
-            .write_read(self.base_address,&cmd_buf, &mut recv_buf)
+            .write_read(self.base_address, &cmd_buf, &mut recv_buf)
             .map_err(Error::Comm)?;
 
         Ok(recv_buf[0])
@@ -143,12 +255,10 @@ where
 // any subsequent i2c writes to any register _other than R0xFE_ is not committed.
 // Subsequently writing the unique pattern 0xBEEF to the R0xFE will unlock registers.
 
-
 /// Used for reading and writing a second byte on registers: aka "Byte-Wise Address register"
 const FOLLOW_UP_ADDRESS: u8 = 0xF0;
 
 // Array format: Wide-VGA, Active 752 H x 480 V
-
 
 /// maximum image frame height (pixels)
 pub const MAX_FRAME_HEIGHT: u16 = 480;
@@ -156,28 +266,45 @@ pub const MAX_FRAME_HEIGHT: u16 = 480;
 pub const MAX_FRAME_WIDTH: u16 = 752;
 
 #[repr(u8)]
-pub enum GeneralRegisters {
+pub enum GeneralRegister {
     ChipVersion = 0x00,
+    /// Control register: used for eg switching config contexts
     Control = 0x07,
+    /// Soft Reset of Logic
     SoftReset = 0x0c,
+    /// High Dynamic Range enable
     HdrEnable = 0x0f,
+    /// ADC Resolution Control
     AdcResCtrl = 0x1c,
+    /// Row Noise Correction Control 1
     RowNoiseCorrCtrl = 0x70,
-    DigitalTest = 0x7f,
+    /// Test pattern storage
+    TestPattern = 0x7f,
+    /// Tiled digital gain
     TiledDigitalGain = 0x80,
-
+    /// Desired luminance
     AgcAecDesiredBin = 0xa5,
+    /// Exposure skip (number of frames to skip between changes in AEC, 0..15)
     AecUpdate = 0xa6,
+    /// AEC Lowpass filter (0..2)
     AecLowpass = 0xa8,
+    /// Gain skip (number of frames to skip between changes in AGC, 0-15)
     AgcUpdate = 0xa9,
+    /// AGC Lowpass filter (0..2)
     AgcLowpass = 0xaa,
-    MaxGain = 0xab,
+    /// AGC Max Gain
+    AgcMaxGain = 0xab,
     /// Minimum coarse shutter width
     MinExposure = 0xac,
     /// Maximum coarse shutter width
     MaxExposure = 0xad,
+    /// AEC/AGC Enable
     AecAgcEnable = 0xaf,
+    /// Histogram pixel count
     AgcAecPixelCount = 0xb0,
+
+    /// Register locking (either All/RW or just RO)
+    RegisterLock = 0xfe,
 }
 
 /// Allows switching quickly between two separate configuration contexts
@@ -188,13 +315,57 @@ pub enum ParamContext {
 }
 
 #[repr(u8)]
-pub enum ContextARegisters {
+pub enum ContextARegister {
     ColumnStart = 0x01,
-    //TODO fill in the rest
+    RowStart = 0x02,
+    WindowHeight = 0x03,
+    WindowWidth = 0x04,
+    /// Horizontal Blanking
+    HBlanking = 0x05,
+    /// Vertical Blanking
+    VBlanking = 0x06,
+    /// Coarse Shutter Width 1
+    CoarseShutter1 = 0x08,
+    /// Coarse Shutter Width 2
+    CoarseShutter2 = 0x09,
+    /// Coarse Shutter Width Control
+    CoarseShutterCtrl = 0x0A,
+    /// Coarse Shutter Width Total
+    CoarseShutterTotal = 0x0B,
+    ReadMode = 0x0D,
+    V1Ctrl = 0x31,
+    V2Ctrl = 0x32,
+    V3Ctrl = 0x33,
+    V4Ctrl = 0x34,
+    /// Analog Gain Control
+    AnalogGainCtrl = 0x35,
+    /// Fine Shutter Width 1
+    FineShutter1 = 0xD3,
+    /// Fine Shutter Width 2
+    FineShutter2 = 0xD4,
+    /// Fine Shutter Width Total
+    FineShutterTotal = 0xD5,
 }
 
 #[repr(u8)]
-pub enum ContextBRegisters {
-    ColumnStart = 0xc9,
-    //TODO fill in the rest
+pub enum ContextBRegister {
+    ColumnStart = 0xC9,
+    RowStart = 0xCA,
+    WindowHeight = 0xCB,
+    WindowWidth = 0xCC,
+    HBlanking = 0xCD,
+    VBlanking = 0xCE,
+    CoarseShutter1 = 0xCF,
+    CoarseShutter2 = 0xD0,
+    CoarseShutterCtrl = 0xD1,
+    CoarseShutterTotal = 0xD2,
+    ReadMode = 0x0E,
+    V1Ctrl = 0x39,
+    V2Ctrl = 0x3A,
+    V3Ctrl = 0x3B,
+    V4Ctrl = 0x3C,
+    AnalogGainCtrl = 0x36,
+    FineShutter1 = 0xD6,
+    FineShutter2 = 0xD7,
+    FineShutterTotal = 0xD8,
 }
